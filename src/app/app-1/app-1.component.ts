b911e4b1596d7b746aa440d148b7c23e327276dc
MarkMizenSagecom <mark.mizen@sage.com>
2020-10-06 15:01:02 +0100

    initial commit

---
+++ src/app/app-1/app-1.component.ts
@@ -0,0 +1,7 @@
+import { Component } from "@angular/core";
+
+@Component({
+  'selector': 'app-1-test',
+  template: ''
+})
+export class App1Component {}
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { EmptyRouteComponent } from './empty-route/empty-route.component'
+import { APP_BASE_HREF } from '@angular/common';
+import { App1Component } from './app-1/app-1.component';
+
+
+const routes: Routes = [
+  { path: 'app1', component: App1Component },
+  { path: '**', component: EmptyRouteComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [
+    { provide: APP_BASE_HREF, useValue: '/' },
+  ],
+})
+export class AppRoutingModule { }
